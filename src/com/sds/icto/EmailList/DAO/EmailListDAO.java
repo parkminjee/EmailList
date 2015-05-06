39f911b37fb7c75dedb65deccade28cc0099bd2e
박민지 <minjee9292@naver.com>
2015-05-06 17:28:53 +0900

    commit

---
+++ src/com/sds/icto/EmailList/DAO/EmailListDAO.java
@@ -0,0 +1,15 @@
+package com.sds.icto.EmailList.DAO;
+
+import java.util.List;
+import com.sds.icto.EmailList.VO.EmailListVO;
+
+public class EmailListDAO {
+	public void insert(){
+		
+	}
+	
+	public List<EmailListVO> fetchList(){
+		
+		return null;
+	}
+}
